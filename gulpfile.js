c2f670ca2ec910e00556b70305813ae787e62a51
Thomas Hampe <thomas@hampe.co>
2014-12-19 20:54:42 +0100

    added css source maps

--- gulpfile.js
+++ gulpfile.js
@@ -22,12 +22,14 @@ gulp.task('default', ['lint:sass', 'sass', 'lint:js', 'js']);
 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(gulp.dest('./www/css/'))
     .pipe(minifyCss({
       keepSpecialComments: 0
     }))
     .pipe(rename({ extname: '.min.css' }))
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./www/css/'))
     .on('end', done);
 });
